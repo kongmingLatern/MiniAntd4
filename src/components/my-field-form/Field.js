0deec6e9ec4cc9353e3f4a7fb6e41b9b11c6becd
施颖杰 <1577310596@qq.com>
2022-07-01 16:13:41 +0800

    feat: add Fields getControlled

--- src/components/my-field-form/Field.js
+++ src/components/my-field-form/Field.js
@@ -1,6 +1,19 @@
 import { Component } from 'react'
 
 export default class Field extends Component {
+  getControlled = () => {
+    return {
+      // getState
+      value: 'omg',
+
+      onChange: (e) => {
+        // setState
+        const newValue = e.target.value
+        console.log('newValue:', newValue);
+      }
+    }
+   }
+
   render() {
     const { children } = this.props
     return children
