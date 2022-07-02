8d9d4e98905f5aa3a8fbe2800e74f452f87b5b18
施颖杰 <1577310596@qq.com>
2022-07-02 21:17:05 +0800

    feat: add a way by using FunctionComponent about Filed

--- src/components/my-field-form/Field.js
+++ src/components/my-field-form/Field.js
@@ -3,29 +3,80 @@ import React, {
 } from 'react'
 import FieldContext from './FieldContext'
 
-export default class Field extends Component {
-  static contextType = FieldContext
+// export default class Field extends Component {
+//   static contextType = FieldContext
 
-  componentDidMount() {
-    this.unregister = this.context.registerFieldEntities(this)
-  }
+//   componentDidMount() {
+//     this.unregister = this.context.registerFieldEntities(this)
+//   }
 
-  componentWillUnmount() {
-    this.unregister()
-  }
+//   componentWillUnmount() {
+//     this.unregister()
+//   }
 
-  onStoreChange = () => {
-    this.forceUpdate()
-  }
+//   onStoreChange = () => {
+//     this.forceUpdate()
+//   }
 
-  getControlled = () => {
-    const {
-      getFieldValue,
-      setFieldsValue
-    } = this.context
-    const {
-      name
-    } = this.props
+//   getControlled = () => {
+//     const {
+//       getFieldValue,
+//       setFieldsValue
+//     } = this.context
+//     const {
+//       name
+//     } = this.props
+//     return {
+//       // getState
+//       value: getFieldValue(name),
+//       onChange: (e) => {
+//         const newValue = e.target.value
+//         // setState
+//         setFieldsValue({
+//           [name]: newValue
+//         })
+//       }
+//     }
+//   }
+
+//   render() {
+//     const {
+//       children
+//     } = this.props
+//     // this.getControlled(): React 元素的 props 属性值
+//     const returnChildNode = React.cloneElement(children, this.getControlled())
+//     return returnChildNode
+//   }
+// }
+
+export default function Field(props) {
+
+  const { children, name } = props
+  const {
+    getFieldValue,
+    setFieldsValue,
+    registerFieldEntities
+  } = React.useContext(FieldContext)
+
+  const [, forceUpdate] = React.useReducer(x => x + 1, 0)
+
+  React.useLayoutEffect(() => {
+    const unregister = registerFieldEntities({
+      props,
+      onStoreChange: forceUpdate
+    })
+  }, [])
+
+
+  // componentDidMount() {
+  //   this.unregister = this.context.registerFieldEntities(this)
+  // }
+
+  // componentWillUnmount() {
+  //   this.unregister()
+  // }
+
+  const getControlled = () => {
     return {
       // getState
       value: getFieldValue(name),
@@ -38,13 +89,7 @@ export default class Field extends Component {
       }
     }
   }
-
-  render() {
-    const {
-      children
-    } = this.props
-    // this.getControlled(): React 元素的 props 属性值
-    const returnChildNode = React.cloneElement(children, this.getControlled())
-    return returnChildNode
-  }
+  // this.getControlled(): React 元素的 props 属性值
+  const returnChildNode = React.cloneElement(children, getControlled())
+  return returnChildNode
 }
